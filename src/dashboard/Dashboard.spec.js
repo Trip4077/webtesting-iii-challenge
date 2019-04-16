78a30ea33892ac0fdeb0851059baad3add0e0248
Trip4077 <bj2336@email.vccs.edu>
2019-04-16 09:45:09 -0400

    Dashboard and Children Render Tests Pass

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,25 @@
-// Test away
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+
+import Dashboard from './Dashboard';
+
+afterEach(cleanup);
+
+describe('<Dashboard />', () => {
+    it('should render', () => {
+        render(<Dashboard />);
+    });
+
+    it('should render a <Display /> component with open display as default', () => {
+        const { getByText } = render(<Dashboard />);
+
+        getByText(/open/i);
+    });
+
+    it('should render a <Control /> component with close button as default', () => {
+        const { getByText } = render(<Dashboard />)
+
+        getByText(/close gate/i)
+    }); 
+})
+
